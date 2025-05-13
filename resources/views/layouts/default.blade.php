27ce1a5a420fc421c03f67a16431b28c228bd9e9
zhanghanwen19 <zhw597426798@gmail.com>
2025-05-13 15:54:32 +0800

    完成拆分 _header.blade.php 和 _footer.blade.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,19 +5,14 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand" href="/">Lu Stormstout</a>
-        <ul class="navbar-nav justify-content-end">
-            <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
-            <li class="nav-item" ><a class="nav-link" href="#">Login</a></li>
-        </ul>
-    </div>
-</nav>
+@include('layouts._header')
 
 <div class="container">
+
     @yield('content')
+
+    @include('layouts._footer')
+
 </div>
 </body>
 </html>
