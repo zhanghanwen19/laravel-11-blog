e577e8722c97e5b94aa29038a983f2bd07a8e419
zhanghanwen19 <zhw597426798@gmail.com>
2025-05-14 16:17:04 +0800

    修改首页链接和标题

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,7 +4,7 @@
     <div class="bg-light p-3 p-sm-5 rounded">
         <h1>Hello 👋</h1>
         <p class="lead">
-            你现在所看到的是 <a href="https://lustormstout.com">LuStormstout's blog</a> 的示例项目主页。
+            你现在所看到的是 <a href="http://localhost:8000">Zhanghanwen's blog</a> 的示例项目主页。
         </p>
         <p>
             一切，将从这里开始。
