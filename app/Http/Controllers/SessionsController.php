43e340e49380db8094d475d6b00a13074e3b253b
zhanghanwen19 <zhw597426798@gmail.com>
2025-05-14 15:44:19 +0800

    实现登录功能

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class SessionsController extends Controller
+{
+    /**
+     * Show the login form.
+     *
+     * @return View
+     */
+    public function create(): View
+    {
+        return view('sessions.create');
+    }
+
+    /**
+     * Log the user in.
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email|max:255',
+            'password' => 'required',
+        ]);
+
+        if (auth()->attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->route('users.show', auth()->user())->with('success', 'Logged in successfully.');
+        }
+
+        return back()->withInput()->with('danger', 'Invalid credentials.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
+use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\StaticPagesController;
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
+// 静态页面
 Route::get('/', [StaticPagesController::class, 'home'])->name('home');
 Route::get('/help', [StaticPagesController::class, 'help'])->name('help');
 Route::get('/about', [StaticPagesController::class, 'about'])->name('about');
 
+// 注册
 Route::get('signup', [UsersController::class, 'create'])->name('signup');
+
+// 用户资源
 Route::resource('users', UsersController::class);
 // GET|HEAD   users ............... users.index › UsersController@index 用户列表
 // POST       users ............... users.store › UsersController@store 新增用户
@@ -17,3 +22,8 @@ Route::resource('users', UsersController::class);
 // PUT|PATCH  users/{user} ...... users.update › UsersController@update 用户更新
 // DELETE     users/{user} .... users.destroy › UsersController@destroy 用户删除
 // GET|HEAD   users/{user}/edit ..... users.edit › UsersController@edit 用户编辑表单
+
+// 登录和退出登录
+Route::get('login', [SessionsController::class, 'create'])->name('login');
+Route::post('login', [SessionsController::class, 'store'])->name('login');
+Route::delete('logout', [SessionsController::class, 'destroy'])->name('logout');
