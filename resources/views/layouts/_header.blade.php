0be81c45b288616ef24689e8c86f3bf41a545a6c
zhanghanwen19 <zhw597426798@gmail.com>
2025-05-20 16:45:23 +0800

    当页面宽度比较小的时候折叠导航栏菜单

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,31 +1,38 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container ">
-        <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name', "ZhangHanwen's Blog") }}</a>
-        <ul class="navbar-nav justify-content-end">
-            @if (Auth::check())
-                <li class="nav-item"><a class="nav-link" href="{{ route('users.index') }}">Users</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">About</a></li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        {{ Auth::user()->name }}
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">个人中心</a>
-                        <a class="dropdown-item" href="{{ route('users.edit', Auth::user()) }}">编辑资料</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" id="logout" href="#">
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name', "Zhanghanwen's Blog") }}</a>
+
+        {{-- Navbar Toggler Button --}}
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        {{-- Collapsible Navbar Content --}}
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0"> {{-- ms-auto 将导航项推向右侧 --}}
+                @if (Auth::check())
+                    <li class="nav-item"><a class="nav-link" href="{{ route('users.index') }}">Users</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">About</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown"> {{-- dropdown-menu-end 使下拉菜单右对齐 --}}
+                            <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">个人中心</a>
+                            <a class="dropdown-item" href="{{ route('users.edit', Auth::user()) }}">编辑资料</a>
+                            <div class="dropdown-divider"></div>
+                            {{-- Logout Form - styled button as dropdown item --}}
                             <form action="{{ route('logout') }}" method="POST">
                                 @csrf
-                                @method('delete')
-                                <button class="btn btn-block btn-danger" type="submit" name="button">Logout</button>
+                                <button class="dropdown-item text-danger" type="submit">Logout</button>
                             </form>
-                        </a>
-                    </div>
-                </li>
-            @else
-                <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">Help</a></li>
-                <li class="nav-item" ><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-            @endif
-        </ul>
+                        </div>
+                    </li>
+                @else
+                    <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">Help</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                @endif
+            </ul>
+        </div>
     </div>
 </nav>
